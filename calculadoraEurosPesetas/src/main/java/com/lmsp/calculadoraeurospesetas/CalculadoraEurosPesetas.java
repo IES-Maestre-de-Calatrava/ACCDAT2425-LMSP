f2da2a48a2e33e48fa6581378691128eb8e55e55
Lucas Serrano Pérez <lmserranop03@educastillalamancha.es>
2024-09-12 14:13:13 +0200

    actualizacion de prueba
    prueba con repositorio

--- calculadoraEurosPesetas/src/main/java/com/lmsp/calculadoraeurospesetas/CalculadoraEurosPesetas.java
+++ calculadoraEurosPesetas/src/main/java/com/lmsp/calculadoraeurospesetas/CalculadoraEurosPesetas.java
@@ -4,6 +4,7 @@
 
 package com.lmsp.calculadoraeurospesetas;
 
+import java.util.Scanner;
 /**
  *
  * @author LMSP by Lucas Manuel Serrano Perez
@@ -15,7 +16,13 @@ public class CalculadoraEurosPesetas {
         System.out.println("1: convertir euros a pesetas");
         System.out.println("2: convertir pesetas a euros");
         System.out.println("0: salir");
+        Scanner sc = new Scanner(System.in);
+        int opcion = sc.nextInt;
+        if (opcion == 1){
+            return calculadoraEuroPeseta(1.0);
+        }
     }
+    
     public static double calculadoraEuroPeseta(double euros){
         return euros*166.386;
     }
