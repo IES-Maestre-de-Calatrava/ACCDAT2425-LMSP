b7db5056ba267a6958848be4624e66db7526ee15
Lucas Serrano Pérez <lmserranop03@educastillalamancha.es>
2024-10-16 14:36:44 +0200

    .

---
+++ R1/FileDOMxml/src/main/java/com/lmsp/maestre/filedomxml/FileDOMxml.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.lmsp.maestre.filedomxml;
+
+import com.lmsp.maestre.filedomxml.model.Empleado;
+import com.lmsp.maestre.filedomxml.model.GestionContenidoDOM;
+import java.util.List;
+import org.w3c.dom.Element;
+
+/**
+ *
+ * @author LMSP by Lucas Manuel Serrano Perez
+ */
+public class FileDOMxml {
+
+    public static void main(String[] args) {
+        GestionContenidoDOM modelo = new GestionContenidoDOM("Empleados");
+        
+//        Element elem = modelo.addNodo("Empleado");
+//        modelo.addNodoYTexto("identificador","1", elem);
+//        
+//        elem = modelo.addNodo("Empleado");
+//        modelo.addNodoYTexto("identificador","2", elem);
+//        
+//        elem = modelo.addNodo("Empleado");
+//        modelo.addNodoYTexto("identificador","3", elem);
+        modelo.cargarArchivoEnMemoria("./resources/Empleados.xml");
+        modelo.mostrarPantalla();
+        //modelo.mostrarPantalla();
+        List<Empleado> emple = modelo.getEmpleados();
+        for(Empleado e:emple){
+            System.out.println(e);
+        }
+        //modelo.generarArchivoDelDom("./resources/Empleados.xml");
+        //paras el borrado usar removeChild();
+    }
+}
